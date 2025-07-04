df4dfd84be8afeebc2820a30140a98bc406075e4
yuvr0131 <yuvr0131@gmail.com>
2025-07-04 13:43:12 +0530

    Fix: Re-added all files after Git LFS model tracking

--- resnet_flask_api/model/predictor.py
+++
@@ -1,8 +0,0 @@
-import torch
-from config import CLASS_NAMES
-
-def predict(model, image_tensor):
-    with torch.no_grad():
-        outputs = model(image_tensor)
-        _, predicted = torch.max(outputs, 1)
-        return CLASS_NAMES[predicted.item()]
